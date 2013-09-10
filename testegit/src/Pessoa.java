bd0719008184376eec1217a9794e48dd10b40118
joaopaulo1000 <joao_paulo_1000@yahoo.com.br>
2013-09-10 13:28:14 -0300

    Primeiro Commit

---
+++ testegit/src/Pessoa.java
@@ -0,0 +1,27 @@
+public class Pessoa {
+
+	private Integer idPessoa;
+	private String nome;
+	private String email;
+	public Integer getIdPessoa() {
+		return idPessoa;
+	}
+	public void setIdPessoa(Integer idPessoa) {
+		this.idPessoa = idPessoa;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+}
