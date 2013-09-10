9949171f653ce3eba3e1c80ae42184ec744c7614
joaopaulo1000 <joao_paulo_1000@yahoo.com.br>
2013-09-10 13:54:27 -0300

    nova classe.

---
+++ testegit/src/PessoaFisica.java
@@ -0,0 +1,21 @@
+
+public class PessoaFisica {
+	private Pessoa pessoa;
+	private String cpf;
+	
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+
+}
