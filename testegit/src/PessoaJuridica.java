8ed5f12d50dee1cbe5b10d3f460fcd69add9f906
joaopaulo1000 <joao_paulo_1000@yahoo.com.br>
2013-09-10 14:59:49 -0300

    Criação da classe PessoaJuridica

---
+++ testegit/src/PessoaJuridica.java
@@ -0,0 +1,21 @@
+
+public class PessoaJuridica {
+	private Pessoa pessoa;
+	private String cnpj;
+	
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+	
+	
+	
+}
